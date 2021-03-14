2ddef1caf7778e600c2339896f82458199d17ef5
lucamouchel <lucamouchel13@gmail.com>
2021-03-14 19:51:10 +0100

    added login capacity with fixed accounts

---
+++ src/sample/SignUpPage.java
@@ -0,0 +1,45 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SignUpPage {
+    @FXML
+    private TextField name;
+    @FXML
+    private TextField lastName;
+    @FXML
+    private DatePicker datePicker;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Label wrongPasswordLabel;
+    @FXML
+    private Label wrongSignUpLabel;
+    @FXML
+    private Button signUpButton;
+
+    public static void openSignUpPage() throws IOException {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(loginPage.class.getResource("SignUpPage.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+            Stage stage = new Stage();
+            stage.setTitle("New Window");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            Logger logger = Logger.getLogger(loginPage.class.getName());
+            logger.log(Level.SEVERE, "Failed to create new Window.", e);
+        }
+    }
+}
---
+++ src/sample/loginPage.java
@@ -0,0 +1,69 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+import static sample.SignUpPage.openSignUpPage;
+
+public class loginPage {
+    private final Account account1 = new Account("Howard", "0123456789");
+    private final Account account2 = new Account("lucathebest", "luca2002");
+    private final Account account3 = new Account("Boobs", "42069");
+    private final List<String> usernames = List.of(account1.getUsername(), account2.getUsername(), account3.getUsername());
+    private final HashMap<Account, String> map = new HashMap<>();
+
+    @FXML
+    private Button contactInfo;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button signin;
+    @FXML
+    private Button signup;
+    @FXML
+    private Label failedSignInLabel;
+
+    public void setLoginButtonAction() throws IOException {
+        if (!(validUsername() && validPassword())) {
+            failedSignInLabel.setText("Username or password is wrong!");
+        } else {
+            failedSignInLabel.setText("");
+        }
+
+    }
+
+    private boolean validUsername() {
+        boolean validUsername = false;
+        if (usernames.stream().anyMatch(x -> username.getText().equals(account1.getUsername())
+                || username.getText().equals(account2.getUsername())
+                || username.getText().equals(account3.getUsername())))
+            validUsername = true;
+        return validUsername;
+    }
+
+    private boolean validPassword() {
+        boolean validPassword = false;
+        map.put(account1, account1.getPassword());
+        map.put(account2, account2.getPassword());
+        map.put(account3, account3.getPassword());
+        boolean match = (username.getText().equals(account1.getUsername()) && password.getText().equals(map.get(account1)))
+                || (username.getText().equals(account2.getUsername()) && password.getText().equals(map.get(account2)))
+                || (username.getText().equals(account3.getUsername()) && password.getText().equals(map.get(account3)));
+        if (match) validPassword = true;
+        return validPassword;
+    }
+
+    public void setSignupButtonAction() throws IOException {
+        openSignUpPage();
+    }
+}
