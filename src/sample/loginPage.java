1353aa8006a54da9f99712415b42ce323b66b28e
lucamouchel <lucamouchel13@gmail.com>
2021-03-15 23:51:37 +0100

    contact info update

--- src/sample/loginPage.java
+++ src/sample/loginPage.java
@@ -2,15 +2,13 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
+import static sample.OpenContactInfo.openInfoPage;
 import static sample.SignUpPage.openSignUpPage;
 
 public class loginPage {
@@ -69,5 +67,9 @@ public class loginPage {
 
     public List<String> getUsernameList(){
         return List.copyOf(usernames);
+    }
+
+    public void setContactInfoButtonAction() throws IOException {
+        openInfoPage();
     }
 }
